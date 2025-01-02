1303755499f7425839f2d52eed6cf0c08ec9551a
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-01-02 19:36:46 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,8 @@
+// settings.js
+module.exports = {
+    owner: ['6285210868214'],
+    prefix: ['/', '.', '!', '#'],
+    botName: 'Qoupay Bot',
+    footer: '© 2025 Qoupay Dev',
+    apiKey: 'YOUR_API_KEY', // Add your API keys here if needed
+};
