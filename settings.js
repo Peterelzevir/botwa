54949cdf41c39c1ff1249f5da624022adb6b1e98
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-01-02 19:40:11 +0700

    Delete settings.js

--- settings.js
+++
@@ -1,8 +0,0 @@
-// settings.js
-module.exports = {
-    owner: ['6285210868214'],
-    prefix: ['/', '.', '!', '#'],
-    botName: 'Qoupay Bot',
-    footer: '© 2025 Qoupay Dev',
-    apiKey: 'YOUR_API_KEY', // Add your API keys here if needed
-};
